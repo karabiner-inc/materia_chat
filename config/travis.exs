103aea75baa18fd39583af5cd6aa1b4de47e2abb
Tuchiro Yoshimura <tcr.yoshimura@karabiner.tech>
2019-09-12 16:02:35 +0900

    fix travis.exs http port

--- config/travis.exs
+++ config/travis.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :materia_chat, MateriaChatWeb.Test.Endpoint,
-  http: [port: 4001],
+  http: [port: 4000],
   #server: false,
   debug_errors: true,
   code_reloader: false,
